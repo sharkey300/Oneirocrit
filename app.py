d41d2bd9efbe7fa34e627db3a83088b9a68d0aa8
sharkey300 <sharkey300@icloud.com>
2024-09-23 19:31:02 -0700

    fix compatibility issue with setting 'json as ascii' config

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from visualize import generate_heatmap, generate_wordcloud
 
 matplotlib.use('Agg')
 app = Flask(__name__)
-app.json.ensure_ascii = False
+app.config['JSON_AS_ASCII'] = False
 
 if not os.path.isdir(PARENT_DIR):
     os.mkdir(PARENT_DIR)
