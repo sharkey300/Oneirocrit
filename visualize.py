c9e97274c5e15476eb8d320fe4ac617bf59e0ff0
sharkey300 <sharkey300@icloud.com>
2024-09-27 10:43:37 -0700

    experimental optimizations

--- visualize.py
+++ visualize.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pandas import DataFrame as df
 from wordcloud import WordCloud
+from concurrent.futures import ThreadPoolExecutor
+from functools import lru_cache
 
 from util.constants import PARENT_DIR
 
 frequency_cache = {}
 
-def load_frequency(show, season = None, episode = None):
+@lru_cache(maxsize=None)
+def load_frequency(show, season=None, episode=None):
     frequency = {}
     if episode:
         file = f'{PARENT_DIR}/{show}/analysis/word_frequency/episode/{season}/{episode}'
@@ -28,8 +31,6 @@ def get_name_of_show(show):
     with open(f'{PARENT_DIR}/{show}/meta/title.txt', 'r', encoding='utf-8') as f:
         return f.readline().strip()
 
-# this code is modified from the code found at:
-#  https://github.com/natebrix/proust/blob/main/proust_names.py
 def name_frequency_plot(df, starts=None, show=None, season=None, transform=None, color_map='Blues', norm=None, stretch_factor=1):
     xy = np.array(df)
     if transform:
@@ -38,8 +39,6 @@ def name_frequency_plot(df, starts=None, show=None, season=None, transform=None,
     plt.rcParams["figure.figsize"] = (8, row_count * stretch_factor)
     height_ratios = [stretch_factor] * row_count
     
-    # thanks stackoverflow:
-    #  https://stackoverflow.com/questions/45841786/creating-a-1d-heat-map-from-a-line-graph
     fig, axs = plt.subplots(nrows=row_count, sharex=True, gridspec_kw={'height_ratios': height_ratios}, constrained_layout=True)
 
     if row_count == 1:
@@ -61,7 +60,6 @@ def name_frequency_plot(df, starts=None, show=None, season=None, transform=None,
             ax.set_title('Frequency of words in ' + (get_name_of_show(show) if show else 'show') + (' Season ' + str(int(season)) if season and int(season) > -1 else '') + ' by episode')
         if ax_i == len(axs) - 1:
             ax.set_xlabel('Season' if starts else 'Episode')
-    # plt.tight_layout()
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
     buf.seek(0)
@@ -74,27 +72,18 @@ def smooth_ref_count(rc, window=3):
     return rcs
 
 def generate_season_labels(starts):
-    labels = []
-    for i in range(len(starts)):
-        labels.append(str(i + 1))
-    return labels
+    return [str(i + 1) for i in range(len(starts))]
 
-def format_word(str):
+def format_word(word):
     try:
-        word, pos = str.split('_')
+        word, pos = word.split('_')
         if pos == 'PROPN':
             return word.capitalize()
         return word
     except:
-        print(str)
+        return word
 
 def get_ref_count_by_episode(words, show, season=None, skipOtherSeasons=False):
-    if (season):
-        key = f'{show}_{season}'
-    else:
-        key = show
-    # if key in frequency_cache:
-    #     return frequency_cache[key]
     ref_count = {}
     count = 0
     starts = []
@@ -104,14 +93,32 @@ def get_ref_count_by_episode(words, show, season=None, skipOtherSeasons=False):
         seasons = os.listdir(f'{PARENT_DIR}/{show}/analysis/word_frequency/episode')
         if skipOtherSeasons:
             seasons = [season for season in seasons if season.isdigit() and int(season) >= 1]
-    for season in seasons:
-        starts.append(count)
-        for episode in os.listdir(f'{PARENT_DIR}/{show}/analysis/word_frequency/episode/{season}'):
-            count += 1
-            episode = episode.split(': ')[0]
-            frequency = load_frequency(show, season = season, episode = episode)
-            ref_count[count] = {word: frequency.get(word, 0) for word in words}
-    frequency_cache[key] = ref_count, starts
+
+    def process_episode(season, episode, episode_index):
+        episode = episode.split(': ')[0]
+        word_keys = [f'{show}_{season}_{episode}_{word}' for word in words]
+        all_cached = all(word_key in frequency_cache for word_key in word_keys)
+        if all_cached:
+            return episode_index, {word: frequency_cache[word_key] for word, word_key in zip(words, word_keys)}
+        else:
+            frequency = load_frequency(show, season=season, episode=episode)
+            result = {word: frequency.get(word, 0) for word in words}
+            for word, word_key in zip(words, word_keys):
+                frequency_cache[word_key] = frequency.get(word, 0)
+            return episode_index, result
+
+    with ThreadPoolExecutor() as executor:
+        futures = []
+        for season in seasons:
+            starts.append(count + 1)
+            episodes = os.listdir(f'{PARENT_DIR}/{show}/analysis/word_frequency/episode/{season}')
+            for episode in episodes:
+                count += 1
+                futures.append(executor.submit(process_episode, season, episode, count))
+        for future in futures:
+            episode_index, result = future.result()
+            ref_count[episode_index] = result
+
     return ref_count, starts
 
 def get_ref_count_by_episode_df(words, show, season=None):
@@ -123,7 +130,7 @@ def get_ref_count_by_episode_df(words, show, season=None):
 
 def generate_heatmap(words, show, season=None, smooth_data=True):
     ref_count, starts = get_ref_count_by_episode_df(words, show, season)
-    if (smooth_data):
+    if smooth_data:
         ref_count = smooth_ref_count(ref_count)
     color_map = 'Greens' if season else 'Blues'
     if len(starts) == 1 and season is None:
@@ -139,11 +146,11 @@ def generate_wordcloud(width, height, show, season=None, episode=None, part=None
     max_words = 200
     if len(frequency) > max_words:
         frequency = {k: frequency[k] for k in list(frequency.keys())[:max_words]}
-    wordcloud = WordCloud(width = int(width), height = int(height),
-                colormap= 'plasma' if episode else 'viridis' if season else 'magma',
-                background_color = 'white',
-                stopwords = None,
-                min_font_size = 10).generate_from_frequencies({format_word(word): frequency[word] for word in frequency})
+    wordcloud = WordCloud(width=int(width), height=int(height),
+                          colormap='plasma' if episode else 'viridis' if season else 'magma',
+                          background_color='white',
+                          stopwords=None,
+                          min_font_size=10).generate_from_frequencies({format_word(word): frequency[word] for word in frequency})
     buf = io.BytesIO()
     wordcloud.to_image().save(buf, format='PNG')
     buf.seek(0)
